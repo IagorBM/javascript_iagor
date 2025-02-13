a2caa3696facc6c783ca441818c7a0e598afcece
IagorBM <iagorbremmmalaguti@gmail.com>
2025-02-13 16:53:17 -0300

    deus me livre de qualquer coisa com java

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Olá mundo!");
+var a = 1.1; /*não precisa declarar mas é bom */
+var b = "Iagor";
+var c = []
+var d = {}
+var e = Date('2025-02-13');
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log( e);
