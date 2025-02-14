22e1070aa262474a919c66bc908c90e9f15b3aee
IagorBM <iagorbremmmalaguti@gmail.com>
2025-02-14 17:05:27 -0300

    teria um coisa de mandar email mas não acompanhei

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-console.log("Olá mundo!");
-var a = 1.1; /*não precisa declarar mas é bom */
-var b = "Iagor";
-var c = []
-var d = {}
-var e = Date('2025-02-13');
+console.log("Olá");
+
+var a = 1.2;
+var b = 'Iagor';
+var c = [];
+var d = {};
 
 console.log(typeof a);
 console.log(typeof b);
 console.log(typeof c);
 console.log(typeof d);
-console.log( e);
+
+const data = [1,2,3,4,5,6,7,8,"teste"];
+console.log(data.length);
+
+for(var i=0;i<data.length;i++){
+    console.log(data[i]);
+}
+
+data.forEach((i,shiw)=>{
+    console.log(i,shiw)
+})
+
+//função logo abaixo
+/*const somaDoisValores ()=>{
+}*/
+
+let temp = [];
+data.forEach((item,index) => {
+    if(item<=5){
+        temp.push(item);
+    }
+});
+
+console.log(temp.length);
